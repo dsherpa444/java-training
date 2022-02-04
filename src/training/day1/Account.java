7287d2be54d29a897b828fef75de5330e42d4a35
dsherpa <dsherpa@mutualink.net>
2022-02-04 13:01:47 -0600

    Initial Comment

---
+++ src/training/day1/Account.java
@@ -0,0 +1,19 @@
+package training.day1;
+
+public class Account {
+    //states
+
+    //account number
+    //account name
+    //kyc data
+    //account type
+    //amount
+
+    //behavior
+    //transfer balance
+    //deposit amount
+    //add interest
+    //withdraw amount
+    //close account
+    //open account
+}
---
+++ src/training/day1/laptop.java
@@ -0,0 +1,19 @@
+package training.day1;
+
+public class laptop {
+   //states
+    //price
+    //brand
+    //operating system
+    //color
+    //ram
+    //hard disk size
+    //weight
+
+    //behaviour
+    //open
+    //close
+
+    //states are variables and behaviors are function, methods
+
+}
